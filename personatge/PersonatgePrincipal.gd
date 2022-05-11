d337c44d2b4435ca8b584249aa30e2c6974e167f
AleeSaanta <101257437+AleeSaanta@users.noreply.github.com>
2022-05-11 12:17:42 +0200

    cambios personaje

--- personatge/PersonatgePrincipal.gd
+++ personatge/PersonatgePrincipal.gd
@@ -4,11 +4,8 @@ var velocitat_base = 100
 var direccio = Vector2.DOWN
 var velocitat = Vector2.ZERO
 var gravetat= Vector2.DOWN * 980
-<<<<<<< Updated upstream
 var velocitat_salt = -400
-=======
-var velocitat_salt = -350
->>>>>>> Stashed changes
+
  
 
 
