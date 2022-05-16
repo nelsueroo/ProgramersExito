5878e5d939c6f797b9267f047f7442d1a8fc7bd0
clarabuyreu <101257247+clarabuyreu@users.noreply.github.com>
2022-05-16 14:10:20 +0200

    escala feta

--- personatge/PersonatgePrincipal.gd
+++ personatge/PersonatgePrincipal.gd
@@ -5,16 +5,24 @@ var direccio = Vector2.DOWN
 var velocitat = Vector2.ZERO
 var gravetat= Vector2.DOWN * 980
 var velocitat_salt = -600
+var escala =  false
  
 
 
 func _physics_process(delta):
-	velocitat += gravetat * delta
-	velocitat = move_and_slide(velocitat, Vector2.UP)
-	
+	if not escala:
+		velocitat += gravetat * delta
+	else:
+		velocitat.y = 0
+	if Input.is_action_pressed("ui_up"):
+		if escala:
+			velocitat.y = -100
+	if Input.is_action_pressed("ui_down"):
+		if escala:
+			velocitat.y = 100
+			
 	
 	velocitat.x = 0
-	velocitat += gravetat * delta
 	if Input.is_action_pressed("ui_right"):
 		velocitat += Vector2.RIGHT * velocitat_base
 	if Input.is_action_pressed("ui_left"):
@@ -46,3 +54,11 @@ func anima(velocitat):
 
 func _on_MortTerra_body_entered(body):
 	get_tree().change_scene("res://Escenes/Nivell1.tscn")
+
+
+func _on_Area2D_body_entered(body):
+	escala = true
+
+
+func _on_Area2D_body_exited(body):
+	escala = false
