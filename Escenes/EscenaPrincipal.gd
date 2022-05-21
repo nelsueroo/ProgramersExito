6082566a71d7d256cb83a56e7e8e6a0a2e97ca36
nelsueroo <101257330+nelsueroo@users.noreply.github.com>
2022-05-21 11:11:21 +0200

    Nose que es aixo

--- Escenes/EscenaPrincipal.gd
+++ Escenes/EscenaPrincipal.gd
@@ -3,8 +3,8 @@ extends Control
 
 
 func _on_Jugar_pressed():
-	get_tree().change_scene("res://Escenes/Nivell1.tscn")
-
+	get_tree().change_scene("res://Escenes/TileMap.tscn")
+	
 
 func _on_Sortir_pressed():
 	get_tree().quit()
